e180e6b8e97e74820e1647b68033e2afb7e21b8a
Joseph Poirier <jdpoirier@gmail.com>
2015-11-19 23:00:03 -0600

    use make

--- src/fcch_detector.cpp
+++ src/fcch_detector.cpp
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2010, Joshua Lackey
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     *  Redistributions of source code must retain the above copyright
  *        notice, this list of conditions and the following disclaimer.
  *
@@ -43,6 +43,7 @@
 
 #include <stdio.h>	// for debug
 #include <stdlib.h>
+#include <fftw3.h>
 
 #include <stdexcept>
 #include <string.h>
