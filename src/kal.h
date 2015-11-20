79b13693fb47c58696dee56fc5e934cee4a52ed7
Joseph Poirier <jdpoirier@gmail.com>
2015-11-19 20:08:55 -0600

    lib files

---
+++ src/kal.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015, Joseph Poirier
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     *  Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *     *  Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef _KAL_H
+#define _KAL_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#else
+#define PACKAGE_VERSION "custom build"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef _WIN32
+#include <unistd.h>
+#include <sys/time.h>
+#endif
+#ifdef D_HOST_OSX
+#include <libgen.h>
+#endif /* D_HOST_OSX */
+#include <string.h>
+
+#include <errno.h>
+
+#include "usrp_source.h"
+#include "fcch_detector.h"
+#include "arfcn_freq.h"
+#include "offset.h"
+#include "c0_detect.h"
+#include "version.h"
+#ifdef _WIN32
+#include <getopt.h>
+#define basename(x) "meh"
+#define strtof strtod
+#endif
+
+#define GSM_RATE (1625000.0 / 6.0)
+
+
+int g_verbosity = 0;
+int g_debug = 0;
+
+#endif
